68be6561bc00b07366a791630e035046051dd0b3
Dennis Sebastian <sienshens1st@gmail.com>
2022-09-25 19:55:13 +0700

    tes

--- ProductRecomendation/Startup.cs
+++ ProductRecomendation/Startup.cs
@@ -63,7 +63,7 @@ namespace ProductRecomendation
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. arios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
