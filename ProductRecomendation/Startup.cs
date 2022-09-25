b77078b549653bc631ca79d425290a07e7abc73e
Dennis Sebastian <sienshens1st@gmail.com>
2022-09-25 19:53:16 +0700

    tes

--- ProductRecomendation/Startup.cs
+++ ProductRecomendation/Startup.cs
@@ -27,7 +27,7 @@ namespace ProductRecomendation
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)//tes
         {
             services.AddRazorPages();
 
